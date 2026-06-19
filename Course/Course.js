eecb15710b5f38ae36b740fbca2f3f1c60c39bbc
santhosha4116-pixel <santhosha4116@gmail.com>
2026-06-20 01:00:01 +0530

    update page

---
+++ Course/Course.js
@@ -0,0 +1,12 @@
+//Selecting the side navbar
+
+
+var hamburger = document.getElementById("hamburger");
+var mobileMenu = document.getElementById("mobileMenu");
+
+hamburger.addEventListener("click", function () {
+    mobileMenu.classList.toggle("open");
+});
+
+
+
