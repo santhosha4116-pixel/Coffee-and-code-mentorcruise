5ccc820f71dfffd80b9e12a838ae9cd178ba0589
santhosha4116-pixel <santhosha4116@gmail.com>
2026-06-21 15:56:06 +0530

    done all

---
+++ Home page/Course.js
@@ -0,0 +1,6 @@
+var hamburger = document.getElementById("hamburger");
+var mobileMenu = document.getElementById("mobileMenu");
+
+hamburger.addEventListener("click", function () {
+    mobileMenu.classList.toggle("open");
+});
